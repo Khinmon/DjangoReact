f6cf403411d3c41c9757b9eea85ad79ec074cf2b
Khinmon <monmyatthu2009@gmail.com>
2023-03-26 12:58:59 +0630

    second.

---
+++ DjangoAPI/EmployeeApp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from EmployeeApp import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('department/', views.departmentApi),
+    path('department/<id>', views.departmentApi),
+
+    path('employee/', views.employeeApi),
+    path('employee/<id>', views.employeeApi),
+
+    path('Employee/SaveFile/', views.SaveFile),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
