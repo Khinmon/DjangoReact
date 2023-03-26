38582154cc2e3ef8495e1b1a84eaaa5eb5975eb2
Khinmon <monmyatthu2009@gmail.com>
2023-03-26 23:07:40 +0630

    added vercel setting.

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3t18*%ss6qdnet#w4j6zo@emnozfgpf0pejpnjfaj-db=%s^f2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
